3590dd36474f8123708448c7c9a07030fae068cd
aadiisawant <adisawant0178@gmail.com>
2024-04-12 09:47:24 +0530

    minor changes

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,32 @@
 const http = require('http')
 const fs = require('fs')
+const url = require("url")
+
 const myServer = http.createServer((req ,res)=>{
-    console.log(req.headers);
-    const log = `${Date.now()}: ${req.url}, New Request Received \n`;
+    // console.log(req.headers);
+    if(req.url === '/favicon.ico') return res.end();
+    const log = `${Date.now()}: ${req.method} ${req.url}, New Request Received \n`;
+    const myUrl = url.parse(req.url, true); //@param parseQueryString, so we make it true
+    // query: [Object: null prototype] { name: 'aditya', userid: '1' }, //output
+    // console.log(myUrl);
     fs.appendFile('./log.txt',log,(err,data)=>{
         if(err){
             console.log(err);
         }else
         {
-            switch(req.url){
+            switch(myUrl.pathname){
                 case '/':
                     res.end('HomePage');
                     break
                 case '/about':
-                    res.end('This is Aditya sawant');
+                    // http://localhost:8001/about?name=aditya&userid=1
+                    const username = myUrl.query.name;
+                    
+                    res.end(`Hi! ${username}`);
                     break
+                case '/search':
+                    const search = myUrl.query.search_query;
+                    res.end("Here is your search result for "+search)    
                 default:
                     res.end("404 Not Found")
             }
