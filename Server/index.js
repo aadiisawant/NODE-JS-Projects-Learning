ecbf947d620d14896d8dd80cc65fa18539214846
aadiisawant <adisawant0178@gmail.com>
2024-04-12 10:33:21 +0530

    installed express and updated the code structure

--- Server/index.js
+++ Server/index.js
@@ -1,41 +1,12 @@
-const http = require('http')
-const fs = require('fs')
-const url = require("url")
+const express = require('express')
 
+const app = express();
 
+app.get('/', (req,res)=>{
+    res.send("Hello! this is Home page")
+})
+app.get('/about',(req,res)=>{
+    res.send(`Hello! ${req.query.name}`)
+} )
 
-function myHandler(req,res){
-     // console.log(req.headers);
-     if(req.url === '/favicon.ico') return res.end();
-     const log = `${Date.now()}: ${req.method} ${req.url}, New Request Received \n`;
-     const myUrl = url.parse(req.url, true); //@param parseQueryString, so we make it true
-     // query: [Object: null prototype] { name: 'aditya', userid: '1' }, //output
-     // console.log(myUrl);
-     fs.appendFile('./log.txt',log,(err,data)=>{
-        if(err){
-             console.log(err);
-        }else
-        {
-            switch(myUrl.pathname){
-                 case '/':
-                     res.end('HomePage');
-                     break
-                 case '/about':
-                     // http://localhost:8001/about?name=aditya&userid=1
-                     const username = myUrl.query.name;
-                     
-                     res.end(`Hi! ${username}`);
-                     break
-                 case '/search':
-                     const search = myUrl.query.search_query;
-                     res.end("Here is your search result for "+search)    
-                 default:
-                     res.end("404 Not Found")
-            }
-        }
-     })
-}
-
-const myServer = http.createServer(myHandler);
-
-myServer.listen(8001, ()=>console.log("Server started..."))
+app.listen(8001, ()=>console.log("Server started..."))
